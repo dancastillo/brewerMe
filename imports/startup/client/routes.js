e06f88ffa87fe393a751c7dbbedb0c1eb3fac670
dancastillo <dancastillo99@gmail.com>
2017-07-21 20:25:36 -0400

    routes.js: added / route to LandingPage template
    Added imports for main and landing-page components

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,2 +1,15 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// import components
+import '../../ui/components/main/main.js';
+import '../../ui/components/landing-page/landing-page.js';
+
+
+// configure public routes
+FlowRouter.route('/', {
+  name: 'LandingPage',
+  action() {
+    BlazeLayout.render('Main', { main: 'LandingPage' });
+  }
+});
