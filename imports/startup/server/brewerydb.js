a6a50719c7ab433178dc7211e263422ee0af0ea0
dancastillo <dancastillo99@gmail.com>
2017-07-26 20:51:48 -0400

    brewerydb.js: added server side file
    Need this server side file to make api calls

---
+++ imports/startup/server/brewerydb.js
@@ -0,0 +1,4 @@
+import { Meteor } from 'meteor/meteor';
+
+const brewerydb = Meteor.settings.public.brewerydb.base;
+const api = Meteor.settings.private.brewerydb;
---
+++ imports/startup/server/index.js
@@ -0,0 +1,1 @@
+import './brewerydb.js';
