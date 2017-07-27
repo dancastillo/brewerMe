176f7264226de5180ad7fc9428c6ff377eb925fd
dancastillo <dancastillo99@gmail.com>
2017-07-26 21:12:02 -0400

    brewerydb.js: tuned import for base url

--- imports/startup/server/brewerydb.js
+++ imports/startup/server/brewerydb.js
@@ -1,4 +1,4 @@
 import { Meteor } from 'meteor/meteor';
 
-const brewerydb = Meteor.settings.public.brewerydb.base;
+const brewerydb = Meteor.settings.public.brewerydb.url;
 const api = Meteor.settings.private.brewerydb;
