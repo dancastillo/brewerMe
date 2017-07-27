eeb6b9b449ebf581cf5ec7055ebdfa18bda08aeb
dancastillo <dancastillo99@gmail.com>
2017-07-26 21:20:56 -0400

    landing-page.js: added template for js
    added helpers and events

--- imports/ui/components/landing-page/landing-page.js
+++ imports/ui/components/landing-page/landing-page.js
@@ -1,1 +1,15 @@
+import { Meteor } from 'meteor/meteor';
+
 import './landing-page.html';
+
+Template.LandingPage.helpers({
+
+});
+
+Template.LandingPage.events({
+  'click .js-search-brewerydb'(event, template) {
+    event.preventDefault();
+    
+    console.log("submit");
+  }
+});
