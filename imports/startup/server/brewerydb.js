d089932c6d5df97248c9d6a7fa8fafe3d692cc96
dancastillo <dancastillo99@gmail.com>
2017-07-31 12:35:11 -0400

    brewerydb.js: added brewerySearch method
    Created brewery search in order to make an API call to brewerydb.com rest api

--- imports/startup/server/brewerydb.js
+++ imports/startup/server/brewerydb.js
@@ -2,3 +2,15 @@ import { Meteor } from 'meteor/meteor';
 
 const brewerydb = Meteor.settings.public.brewerydb.url;
 const api = Meteor.settings.private.brewerydb;
+
+// Testing
+const barNewHaven = "locations?postalCode=06511&key=9519c7293d178720517f726335e7ba34&format=json";
+
+Meteor.methods({
+	brewerySearch: function() {
+		HTTP.call( 'GET' , brewerydb+barNewHaven, {}, (error, response) => {
+			console.log(error);
+			console.log(response);
+		});
+	}
+});
