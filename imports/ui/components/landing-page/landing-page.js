a0bafe3b5af404d49ef8e2f68f1b10b3db0fe148
dancastillo <dancastillo99@gmail.com>
2017-08-01 13:04:48 -0400

    landing-page.js: clean up with helpler functions
    helper funcitons cleans up code

--- imports/ui/components/landing-page/landing-page.js
+++ imports/ui/components/landing-page/landing-page.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-
+import { formatSearch,
+         replaceSpaces } from '/imports/utils/helpers'
 import './landing-page.html';
 
 Template.LandingPage.helpers({
@@ -10,12 +11,15 @@ Template.LandingPage.events({
   'click .js-search-brewerydb'(event, template) {
     event.preventDefault();
     
-    const search = template.find('#searchInput').value;
+    let search = template.find('#searchInput').value;
     const preference = template.find('#searchPreference').value;
+		const searchFormat = formatSearch(search);
 		
-		const searchUrl = `locations?${preference}=${search}`
-
-		Meteor.call('brewerySearch', (error, response) => {
+		const searchUrl = `locations?${preference}=${searchFormat}`;
+		const url = replaceSpaces(searchUrl);
+		
+		console.log(url)
+		Meteor.call('brewerySearch', url, (error, response) => {
 			console.log(error);
 			console.log(reponse);
 		});
