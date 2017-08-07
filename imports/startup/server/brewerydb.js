5e04074dc02d17b9ac481d165e39ad9b531dee98
dancastillo <dancastillo99@gmail.com>
2017-08-07 12:30:49 -0400

    brewerydb.js: fibers/future to return response from http

--- imports/startup/server/brewerydb.js
+++ imports/startup/server/brewerydb.js
@@ -3,13 +3,28 @@ import { Meteor } from 'meteor/meteor';
 const brewerydb = Meteor.settings.public.brewerydb.url;
 const api = Meteor.settings.private.brewerydb;
 
+const future = require('fibers/future');
 
 Meteor.methods({
 	brewerySearch: function(location) {
 		const url = `${brewerydb}${location}${api}`;
+    const fut = new future();
+    
 		HTTP.call( 'GET', url, {}, (error, response) => {
-			console.log(error);
-			console.log(response);
+      if(error) {
+        console.log(error);
+        fut.return(error);
+      } else {
+        if (response.statusCode === 200) {
+            fut.return(response.content);
+        } else {
+          console.log('Status code = ' + response.statusCode)
+        }
+        
+      }
 		});
+    
+    const result = fut.wait();
+    return result;
 	}
 });
