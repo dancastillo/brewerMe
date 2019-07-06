2cecd16573192cb0e3a8461a10d85936ce4d4de4
dancastillo <dancastillo99@gmail.com>
2019-07-06 19:33:04 -0400

    GH-7: added getAllBeers axios API call

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import brewerController from '../controllers/brewer.controller'
 import locationController from '../controllers/location.controller'
+import beerController from '../controllers/beer.controller'
 
 const router = express.Router();
 
 router.use('/brewers', brewerController);
 
 router.use('/location', locationController);
+
+router.use('/beer', beerController);
 
 export default router;
