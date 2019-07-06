c33b9337c5d796989907d506e0fc55b66f85778e
dancastillo <dancastillo99@gmail.com>
2019-07-05 21:35:23 -0400

    GH-8: routes: added route for locations

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import brewerController from '../controllers/brewer.controller'
+import locationController from '../controllers/location.controller'
 
 const router = express.Router();
 
 router.use('/brewers', brewerController);
+
+router.use('/location', locationController);
 
 router.get('/', (req, res) => {
   res.send('works');
