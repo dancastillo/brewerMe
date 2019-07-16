b6a3af6f27e095a33a2728a2a350e5fc5ff691ce
dancastillo <dancastillo99@gmail.com>
2019-07-15 21:48:26 -0400

    GH-11: add location route in controller

--- api/controllers/location.controller.js
+++ api/controllers/location.controller.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getLocation } from '../routes/beer/location';
+import { getLocation, addLocation } from '../routes/beer/location';
 
 const locationController = express.Router();
 
 locationController.get('/', getLocation);
+
+locationController.post('/', addLocation);
 
 export default locationController;
