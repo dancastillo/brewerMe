8298e32acc87acc180e3ca25f333396b4133b15d
dancastillo <dancastillo99@gmail.com>
2020-07-02 21:48:03 -0400

    GH-43: added routes for api

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,16 @@
 import { Request, Response, Router } from "express";
 import * as location from "../controllers/location";
+import * as brewery from "../controllers/brewery";
+import * as beer from "../controllers/beer";
 
 const router: Router = Router();
 
 router.get("/location",  location.getLocation);
+router.get("/location/:id",  location.getLocationById);
+router.get("/brewery",  brewery.getBrewery);
+router.get("/brewery/:id",  brewery.getBreweryById);
+router.get("/beer",  beer.getBeers);
+router.get("/beer/:id",  beer.getBeerById);
 
 /**
  * @param {Request} req
