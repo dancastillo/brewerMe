963b1f3dec5c9e6179c4db80bcf5e50ce241c63e
dancastillo <dancastillo99@gmail.com>
2020-07-09 20:25:02 -0400

    [GH-34] updated controllers for beer brewery and location

--- server/controllers/beer.ts
+++ server/controllers/beer.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from "express";
+import { getBeers, getBeerById } from "../api/beerApi/api";
+import { IAxiosResponse } from "../api/beerApi/interfaces/IAxiosResponse";
+import { handleAxiosResponse } from "../utils/handle-axios-response";
+import { IApiResponse } from "../interfaces/IApiResponse";
 
 /**
  * @param {Request} req
  * @param {Response} res
  * @return Response
  */
-export const getBeers = (req: Request, res: Response) => {
-    res.send("get 100 beers");
+export const get = async (req: Request, res: Response) => {
+    try {
+        const results: IAxiosResponse = await getBeers();
+        const response: IApiResponse = handleAxiosResponse(results);
+
+        res.send(response);
+    } catch (err) {
+        res.send(err);
+    }
 };
 
 /**
@@ -14,6 +25,14 @@ export const getBeers = (req: Request, res: Response) => {
  * @param {Response} res
  * @return Response
  */
-export const getBeerById = (req: Request, res: Response) => {
-    res.send("get beer by id");
+export const getById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const results: IAxiosResponse = await getBeerById(id);
+        const response: IApiResponse = handleAxiosResponse(results);
+
+        res.send(response);
+    } catch (err) {
+        console.log(err);
+    }
 };
