beea21b3ff89001bc85fcde23f1974cd4bb0c696
dancastillo <dancastillo99@gmail.com>
2020-08-06 20:07:21 -0400

    [GH-29] added npm packages: bootstrap, formik, yup, redux, sass, styled-componets, react-bootstrap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import React, { ReactElement } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const IndexPage: React.FC = (): ReactElement => {
     return (
-        <div>Hello world.</div>
+        <>
+            <Container className="container-fluid">
+                <Row className="pt-5">
+                    <Col className="offset-3 col-3">
+                        <div>Hello world.</div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 };
 
