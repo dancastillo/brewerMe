eb3e0f7cefee51d867e7f67a2647426113c74722
dancastillo <dancastillo99@gmail.com>
2020-08-19 08:30:10 -0400

    [GH-54] added back button component to get to home search page

---
+++ src/components/back-button/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function BackButton() {
+    return (
+        <div className="row">
+            <div className="col-12 text-left">
+                <a
+                    href="/"
+                    className="btn btn-primary m-3"
+                >
+                    Back to Search
+                </a>
+            </div>
+        </div>
+    );
+}
