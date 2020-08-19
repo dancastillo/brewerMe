d743096c9c4115dc8adfe8a424e0028d3acc9103
dancastillo <dancastillo99@gmail.com>
2020-08-19 08:45:05 -0400

    [GH-54] updated search compoennt and styling

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -3,6 +3,7 @@ import { Form, Formik } from "formik";
 import { object, string } from "yup";
 import { Button, Row } from "react-bootstrap";
 import FormInput from "../form/formInput";
+import Router from "next/router";
 
 export default function Search() {
     const initialValidationSchema = {
@@ -30,23 +31,41 @@ export default function Search() {
     };
 
     const inputs = [
-        {name: "searchValue", type: "text", label: "", placeholder: "Search for brewery, beer, by location", inputType: "TextInput"},
+        {name: "searchValue", type: "text", label: "", placeholder: "Search for brewery, beer, by location", inputType: "text"},
         {name: "searchBy", type: "select", label: "searchOptions", placeholder: "Choose an option...", inputType: "select"}
     ];
 
     const additionInputs = [
-        {name: "city", type: "text", label: "", placeholder: "City...", inputType: "TextInput"},
+        {name: "city", type: "text", label: "", placeholder: "City...", inputType: "text"},
         {name: "state", type: "select", label: "states", placeholder: "Choose a State...", inputType: "select"}
     ];
+
+    interface ISearchForm {
+        searchValue: string;
+        searchBy: "brewer" | "beer" | "location";
+        city: string;
+        state: string;
+    }
+
+    const submitSearch = ({searchValue, searchBy, city, state }: ISearchForm) => {
+        if (searchBy === "location") {
+            Router.push(`/${searchBy}/${city}-${state}`);
+        } else {
+            Router.push(`/${searchBy}/${searchValue}`);
+        }
+    };
 
     return (
         <Formik
             validationSchema={object(initialValidationSchema)}
             initialValues={initialValues}
-            onSubmit={(form) => { console.log(form); }}>
+            onSubmit={(values: ISearchForm) => {
+                console.log(JSON.stringify(values, null, 2));
+                submitSearch(values);
+            }}>
             {({ values, isSubmitting }) => (
                 <Row>
-                    <Form className="p-3 mt-5 mb--3">
+                    <Form className="p-3 mt-5 mb--3 offset-md-3 col-md-6 col-sm-12">
                         {inputs.map((input) => {
                             return (
                                 <FormInput
