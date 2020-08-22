dee34f079b1c11e2264449501fd39c9cf9ed3c28
dancastillo <dancastillo99@gmail.com>
2020-08-22 15:46:15 -0400

    [GH-56] added location.get function call for front end

--- server/controllers/location.ts
+++ server/controllers/location.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
-import { getLocationById, getLocationsNearby } from "../api/beerApi/api";
+import { getLocationById } from "../api/beerApi/api";
 import { IAxiosResponse } from "../api/interfaces/IAxiosResponse";
 import { handleAxiosResponse } from "../utils/handle-axios-response";
 import { IApiResponse } from "../interfaces/IApiResponse";
-import { getLocation } from "../api/locationApi/locationApi";
+import { encodeLocationQueryParam } from "../utils/encode-query-param";
+import { ICoordinates } from "../interfaces/ICoordinates";
+import { getLocationAddress, getNearbyByLatLng } from "../utils/location";
+
+/**
+ * @param {Request} req
+ * @param {Response} res
+ * @return Response
+ */
+export const get = async (req: Request, res: Response) => {
+    try {
+        const { city, state } = req.query;
+        const address = encodeLocationQueryParam(`${city}, ${state}`);
+
+        const coordinates: ICoordinates = await getLocationAddress(address);
+        const response: IApiResponse = await getNearbyByLatLng(coordinates.lat, coordinates.lng);
+
+        res.send(response);
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 /**
  * @param {Request} req
@@ -30,8 +51,7 @@ export const getById = async (req: Request, res: Response) => {
 export const getNearby = async (req: Request, res: Response) => {
     try {
         const { latitude, longitude } = req.params;
-        const results: IAxiosResponse = await getLocationsNearby(+latitude, +longitude);
-        const response: IApiResponse = handleAxiosResponse(results);
+        const response = await getNearbyByLatLng(+latitude, +longitude);
 
         res.send(response);
     } catch (err) {
@@ -49,10 +69,8 @@ export const getLatitudeLongitude = async (req: Request, res: Response) => {
         const { address } = req.query;
 
         if (typeof address === "string") {
-            const results: IAxiosResponse = await getLocation(address);
-            const response: IApiResponse = handleAxiosResponse(results);
-
-            res.send(response.data);
+            const result = await getLocationAddress(address);
+            res.send(result);
         } else {
             throw Error("Query Param 'address' should be a string");
         }
